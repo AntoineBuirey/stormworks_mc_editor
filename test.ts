7cdeee11ad07d7b194713501583d30121e180d19
Antoine Buirey <91411073+AntoineBuirey@users.noreply.github.com>
2026-02-22 23:52:47 +0100

    cleaning up test.ts

--- test.ts
+++ test.ts
@@ -6,29 +6,15 @@ export class EngineMonitor extends sw.MicroController {
 
     setup() {
         const engineTemp = sw.InputNumber('Engine Temp');
-        const manualOverride = sw.InputBoolean('Override');
-
+        
         const tempLimit = sw.ConstantNumber(120);
-        this.show_number(engineTemp, 'Engine Temp');
         const isOverheating = sw.GreaterThan(engineTemp, tempLimit);
-        this.show_number(tempLimit, 'Temp Limit');
+
+        
+        const manualOverride = sw.InputBoolean('Override');
         const triggerAlarm = sw.OR(isOverheating, manualOverride);
 
-        let compositeInput = sw.CompositeWrite(1, undefined, engineTemp);
-        compositeInput = sw.CompositeWrite(1, compositeInput, manualOverride);
-
-        const [scriptoutdata, scriptoutvideo] = sw.LuaScript(
-            'test.lua',
-            compositeInput
-        );
-
-        this.show_number(sw.CompositeReadNumber(scriptoutdata, 1), 'Lua Engine Temp');
-
         sw.Output('Alarm Light', "", triggerAlarm);
-    }
-
-    show_number(value: sw.NumberSignal, label: string) {
-        sw.TooltipNumber(label, 'Always', value);
     }
 
 }
